db9689958107217d469d53d9de5aafb25560adb3
arvee0220 <rowellverdejo@gmail.com>
2025-04-24 08:51:57 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+import { keyboard, mouse, Key, screen, left, Point } from "@nut-tree-fork/nut-js";
+
+async function automateKeyAndMouseMovement() {
+    const random = Math.random() * 1000;
+    const random2 = Math.random() * 1000;
+    const randomx = Math.ceil(random);
+    const randomy = Math.ceil(random2);
+
+    keyboard.config.autoDelayMs = 100;
+
+    await keyboard.pressKey(Key.LeftControl);
+    await keyboard.pressKey(Key.LeftShift);
+    await keyboard.pressKey(Key.Tab);
+    await keyboard.releaseKey(Key.Tab);
+    await keyboard.pressKey(Key.Tab);
+    await keyboard.releaseKey(Key.Tab);
+    await keyboard.releaseKey(Key.LeftControl);
+    await keyboard.releaseKey(Key.LeftShift);
+
+    await keyboard.pressKey(Key.LeftSuper);
+
+    await keyboard.pressKey(Key.Down);
+    await keyboard.releaseKey(Key.Down);
+
+    await keyboard.pressKey(Key.Up);
+    await keyboard.releaseKey(Key.Up);
+
+    await keyboard.releaseKey(Key.LeftSuper);
+
+    await mouse.move([new Point(randomx, randomy)]);
+}
+
+async function automateMouseMovement() {
+    const random = Math.random() * 1000;
+    const random2 = Math.random() * 1000;
+    const randomx = Math.ceil(random);
+    const randomy = Math.ceil(random2);
+
+    const currentPosition = await mouse.getPosition();
+
+    await mouse.move([new Point(currentPosition.x + randomx, currentPosition.y + randomy)]);
+}
+
+setInterval(() => {
+    automateKeyAndMouseMovement().catch((e) => console.log(e));
+}, 4000);
+
+setInterval(() => {
+    automateMouseMovement().catch((e) => console.log(e));
+}, 6000);
